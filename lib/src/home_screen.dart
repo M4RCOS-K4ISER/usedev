8dd17617be7d5b5d11d9a1bafb0d17305d47aaa0
Marcos Kaiser <N4TSUNAGI@OUTLOOK.COM>
2025-09-02 21:38:04 -0300

    Add files via upload

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,29 +1,61 @@
 import 'package:flutter/material.dart';
+import 'models/category.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  final List<Category> categories = const [
+    Category(name: 'Eletrônicos', icon: Icons.devices),
+    Category(name: 'Roupas', icon: Icons.checkroom),
+    Category(name: 'Esportes', icon: Icons.sports_soccer),
+    Category(name: 'Móveis', icon: Icons.chair),
+    Category(name: 'Livros', icon: Icons.book),
+    Category(name: 'Beleza', icon: Icons.brush),
+  ];
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-        title: Image.asset('assets/logo.png', height: 40),
+        title: Image.asset('assets/logo.png', height: 40, errorBuilder:
+            (context, error, stackTrace) {
+          return const Text("UseDev");
+        }),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.person_2_outlined)),
           IconButton(
-            icon: Icon(Icons.shopping_cart_outlined),
+              onPressed: () {}, icon: const Icon(Icons.person_2_outlined)),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart_outlined),
             onPressed: () {},
           ),
         ],
       ),
-      body: Placeholder(),
+      drawer: Drawer(
+        child: ListView.builder(
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            final category = categories[index];
+            return ListTile(
+              leading: Icon(category.icon, size: 30, color: Colors.deepPurple),
+              title: Text(category.name),
+              onTap: () {
+                Navigator.pop(context);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Clicou em ${category.name}')),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Tela Principal',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
     );
   }
 }
